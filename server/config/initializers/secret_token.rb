78e2dbd9fd81436e89b207bd6451c97b5ca3f500
David Ruan <ruanwz@gmail.com>
2011-02-05 14:46:12 +0800

    init server using templater

---
+++ server/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Server::Application.config.secret_token = '074d4ee05e3c2ca8c29a3d2c415a052de95a1fdf15492e58ea5fd558ed93cc0e996bbb8decdfa06577e8acef2b86c39eeb35304e477ee0fdc9bfef5bbc0fce51'
